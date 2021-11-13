b4db6adac1ce132e61002f09df89cd82d3bfd939
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-11-13 20:39:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ handler.intial_configurations(True , True , '/content/_master') # mount , detail
 
 def hardTask():
     x = 3
-    for i in range(10000000):
+    for i in range(100000):
         x = x * i + i 
 
 configure.use_gpu(hardTask ) 
