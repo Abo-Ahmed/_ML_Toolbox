c614d1b51e6195cf7c10568d25ba53abcccbf82d
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-11-14 10:38:17 +0200

    Update class_lstm.py

--- model/class_lstm.py
+++ model/class_lstm.py
@@ -6,7 +6,7 @@ class lstm(basic_model):
         self.model = Sequential()
         temp = tf.keras.layers.LSTM(
             units = 2,
-            input_shape=( 512 , 512),
+            input_shape=(5 , 512 , 512),
             activation="tanh",
             recurrent_activation="sigmoid",
             use_bias=True,
