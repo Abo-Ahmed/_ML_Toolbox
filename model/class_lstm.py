4abae38a339ce5a059ff1ad44342e04e6419b2b4
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-11-14 09:56:10 +0200

    Update class_lstm.py

--- model/class_lstm.py
+++ model/class_lstm.py
@@ -41,9 +41,9 @@ class lstm(basic_model):
         handler.test_x = self.batchizeData(handler.test_x , SequenceLength )
         handler.test_y = self.batchizeData(handler.test_y , SequenceLength )
 
-        handler.train_x = handler.train_x.reshape((len(handler.train_x), 5, 512))
-        handler.train_y = handler.train_y.reshape((len(handler.train_y), 5, 512))
-
+        print(handler.train_x)
+        print(handler.train_x.shape)
+        
     def batchizeData(self , dataList ,  batchSize ):
         batches = []
         for i in range(len(dataList) // batchSize ):
