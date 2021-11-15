c99cb05a43e6e406bdc45c8e0760b23a8e2ffcd7
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-11-15 11:46:54 +0200

    *

--- handler/class_BasicModel.py
+++ handler/class_BasicModel.py
@@ -21,38 +21,27 @@ class BasicModel (object):
         print("XXX deleted, model " + self.name)
 
     ################
-    ## model construction
+    ## model details
     ################
     def build (self):
         print('>>> bulding ' + self.name + ' model ...')
 
-    def load_model(self):
-        print('>>> loading ' + self.name + ' model ...')
+    def summery_plot(self , details = True, plotted = True):
         try:
-            self.model = tf.keras.models.load_model(self.path)
+            if(details):
+                self.summery()
+            if(plotted):
+                self.plot()
         except Exception as e:
-            print('XXX failed loading model: ' + self.name  , e)
-
-    def load_weights(self):
-        print('>>> loading ' + self.name + ' weights ...')
-        try:
-            # odd only retrieved - even for backup
-            self.model.load_weights(self.path + "odd-" + self.name + ".h5")
-        except Exception as e:
-            print('XXX failed loading weights for: ' + self.name  ,e)
+            print("XXX Error executing summery_plot model")
             
-    def load_parameters(self):
-        print('>>> loading ' + self.name + ' parameters ...')
-        try:
-            file = open(self.path + "params-" + self.name + ".pkl", "rb")
-            dir = pickle.load(file)
-            self.name = dir["name"]
-            self.loopEpochs = int(dir["loopEpochs"])
-            self.loopIndex = int(dir["loopIndex"]) 
-            self.loopLimit = int(dir["loopLimit"])
-            print(self.name , self.loopEpochs , self.loopIndex , self.loopLimit)
-        except Exception as e:
-            print('XXX failed loading parameters for: ' + self.name  , e)
+    def plot(self):
+        print('>>> plotting model: ' + self.name)
+        tf.keras.utils.plot_model(self.model, to_file=self.name + '.png', show_shapes=True)
+
+    def summery(self):
+        print('>>> showing ' + self.name + ' summery ...')
+        self.model.summary()
 
     ################
     ## model operations
@@ -105,11 +94,59 @@ class BasicModel (object):
         print('>>> predict_y:' , handler.predict_y)
 
     ################
-    ## saving model
+    ## loading and saving model
     ################
+
+    def load_model(self):
+        print('>>> loading ' + self.name + ' model ...')
+        try:
+            self.model = tf.keras.models.load_model(self.path)
+        except Exception as e:
+            print('XXX failed loading model: ' + self.name  , e)
+
+    def save_model(self):
+        print('>>> saving ' + self.name + ' model ...')
+        try:
+            self.model.save(self.path) # saving the entire model
+        except Exception as e:
+            print('XXX failed saving ' + self.name + ' model ...' , e)
+
+    def load_weights(self):
+        print('>>> loading ' + self.name + ' weights ...')
+        try:
+            # odd only retrieved - even for backup
+            self.model.load_weights(self.path + "odd-" + self.name + ".h5")
+        except Exception as e:
+            print('XXX failed loading weights for: ' + self.name  ,e)
+            
+    def checkpoint(self, checkpoint_path = "cp.ckpt"): # saves the best weights
+        return tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+    def save_weights(self, title = ""): # saves the current weights
+        print('>>> saving ' + self.name + ' weights ...')
+        try:
+            self.model.save_weights(self.path + title + "-" + self.name + ".h5" ) # saving weights only
+        except Exception as e:
+            print('XXX failed saving ' + self.name + ' weights ...' , e)
+
+    def load_parameters(self):
+        print('>>> loading ' + self.name + ' parameters ...')
+        try:
+            file = open(self.path + "params-" + self.name + ".pkl", "rb")
+            dir = pickle.load(file)
+            self.name = dir["name"]
+            self.loopEpochs = int(dir["loopEpochs"])
+            self.loopIndex = int(dir["loopIndex"]) 
+            self.loopLimit = int(dir["loopLimit"])
+            print(self.name , self.loopEpochs , self.loopIndex , self.loopLimit)
+        except Exception as e:
+            print('XXX failed loading parameters for: ' + self.name  , e)
+
     def save_parameters(self,index):
         print('>>> saving ' + self.name + ' parameters with index: ' + str(index))
-        print(self.name , self.loopEpochs , self.loopIndex , self.loopLimit)
+        print(self.name , self.loopEpochs , index , self.loopLimit)
         dic =   {
                     "name": self.name,
                     "loopEpochs": self.loopEpochs ,
@@ -119,42 +156,3 @@ class BasicModel (object):
         file = open(self.path + "params-" + self.name + ".pkl" , "wb")
         pickle.dump(dic, file)
         file.close()        
-        
-    def save_weights(self, title = ""): # saves the current weights
-        print('>>> saving ' + self.name + ' weights ...')
-        try:
-            self.model.save_weights(self.path + title + "-" + self.name + ".h5" ) # saving weights only
-        except Exception as e:
-            print('XXX failed saving ' + self.name + ' weights ...' , e)
-
-    def save_model(self):
-        print('>>> saving ' + self.name + ' model ...')
-        try:
-            self.model.save(self.path) # saving the entire model
-        except Exception as e:
-            print('XXX failed saving ' + self.name + ' model ...' , e)
-
-    def checkpoint(self, checkpoint_path = "cp.ckpt"): # saves the best weights
-        return tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
-
-    ################
-    ## model detials
-    ################
-    def summery_plot(self , details = True, plotted = True):
-        try:
-            if(details):
-                self.summery()
-            if(plotted):
-                self.plot()
-        except Exception as e:
-            print("XXX Error executing summery_plot model")
-            
-    def plot(self):
-        print('>>> plotting model: ' + self.name)
-        tf.keras.utils.plot_model(self.model, to_file=self.name + '.png', show_shapes=True)
-
-    def summery(self):
-        print('>>> showing ' + self.name + ' summery ...')
-        self.model.summary()
--- model/class_cnnFunctional.py
+++ model/class_cnnFunctional.py
@@ -1,14 +1,17 @@
 
+from os import name
+
+
 class CnnFunctional(BasicModel): 
 
     def build (self):
         super().build()
         self.model = tf.keras.Sequential()
-        inputs = Input(shape=(512, 512, 3))
-        t = layers.Flatten() (inputs)
-        t = layers.Dense(512, activation=tf.nn.relu) (t)
-        t = layers.Dropout(0.2) (t)
-        outputs = layers.Dense(10, activation=tf.nn.softmax) (t)
+        inputs = Input(shape=(512, 512, 3), name="input_1")
+        t = layers.Flatten(name="flatten_2") (inputs)
+        t = layers.Dense(512, activation=tf.nn.relu , name="dense_3") (t)
+        t = layers.Dropout(0.2,name="dropout_4") (t)
+        outputs = layers.Dense(10, activation=tf.nn.softmax,name="dense_5") (t)
         
         self.model = Model(inputs , outputs)
         self.model.compile(optimizer='adam',
