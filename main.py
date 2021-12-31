5904fccc363c1803df08bef5c4680b79057021ab
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-12-31 20:18:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 # colab link
 # https://colab.research.google.com/drive/1QA3ufl7lur76WzBhuwerLdyjLj-8hoHC?authuser=2
 
+
 print(">>> main module loaded ...")
 execfile('/content/_master/handler/handler.py')
 handler.intial_config(True , True , '/content/_master') # mount , details , project path
 handler.dataset_config( handler.ePath , 224 , 224 , 30 , 1 , True) # path ,width , height , batchSize , startBatch , isColored
 
-
+folder.download_image(33 ,100)
 
 # # print(rating.values[1315650])
 # handler.read_real('/content/drive/MyDrive/eng-mahmoud/dataSet/danbooru2019/images/512px/0000')
