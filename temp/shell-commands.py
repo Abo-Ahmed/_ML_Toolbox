678f2c28a793e7f2c44b7e5864964483a29ac75e
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-12-31 17:09:31 +0200

    Update shell-commands.py

--- temp/shell-commands.py
+++ temp/shell-commands.py
@@ -1,9 +1,10 @@
 
 !cp "/content/drive/My Drive/TAI-sorted.zip"  "/content/json"
-!cp "/content/drive/My Drive/TAI-images.zip"  "/content/images"
-!cp "/content/drive/My Drive/e_images.zip"  "/content/images"
 
+## unzip metadate file
+!tar -xvf  '/content/drive/My Drive/dataSet/danbooru2019/metadata.json.tar.xz' -C '/content/drive/My Drive/dataSet/danbooru2019/metadata'
 !unzip "/content/drive/My Drive/TAI-important.zip" -d "/content"
+
 !zip -r "e_images.zip" "/content/e_img" 
 
 
@@ -11,9 +12,6 @@
 for x in range(99,100):
   !rsync --recursive --verbose rsync://78.46.86.149:873/danbooru2019/original/00{x} "/content/drive/My Drive/dataSet/danbooru2019/original"
   
-## unzip metadate file
-!tar -xvf  '/content/drive/My Drive/dataSet/danbooru2019/metadata.json.tar.xz' -C '/content/drive/My Drive/dataSet/danbooru2019/metadata'
-
 
 # kaggle
 !pip install -q kaggle
