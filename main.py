873fc29f5984bb55b60f8f2dbab3e674e2013384
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-01-02 22:23:08 +0200

    good

--- main.py
+++ main.py
@@ -5,14 +5,19 @@
 
 print(">>> main module loaded ...")
 execfile('/content/_master/handler/handler.py')
-handler.intial_config(True , True , '/content/_master') # mount , details , project path
-handler.dataset_config( handler.sPath , 224 , 224 , 30 , 1 , True) # path ,width , height , batchSize , startBatch , isColored
+# mount , details , project path
+handler.intial_config(True, True, '/content/_master')
+# path ,width , height , batchSize , startBatch , isColored
+handler.dataset_config(handler.sPath, 224, 224, 30, 1, True)
 
-folder.collect_names_patch()
+print("safe" + str(len(rating.safe)))
+print("questionable" + str(len(rating.questionable)))
+print("explicit" + str(len(rating.explicit)))
+
 
 # # print(rating.values[1315650])
 # handler.read_real('/content/drive/MyDrive/eng-mahmoud/dataSet/danbooru2019/images/512px/0000')
-# # handler.read_sample( None)  
+# # handler.read_sample( None)
 
 # # handler.run_models(["CnnSeq"      , "CnnFunctional" , "ResNet" , "Vgg16"  , "vgg16Seq" ,
 # #                     "VggLstm"     , "LstmConv2d"    , "Lstm"   , "LstmBi" , "VggBiLstm" ] )
