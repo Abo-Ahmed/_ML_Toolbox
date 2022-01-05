62627625f900a58ae5eb104dd3a5d382db78f064
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-01-05 22:41:10 +0200

    Update class_AlexNet.py

--- model/class_AlexNet.py
+++ model/class_AlexNet.py
@@ -8,7 +8,7 @@ class AlexNet(BasicModel):
         self.model = tf.keras.models.Sequential()
 
         # Layer 1
-        self.model.add(keras_layers.Conv2D(96, (11, 11), input_shape=img_shape,padding='same', kernel_regularizer=l2(l2_reg)))
+        self.model.add(keras_layers.Conv2D(96, (11, 11), input_shape=(self.imageWidth ,self.imageHeight , self.channels),padding='same', kernel_regularizer=l2(l2_reg)))
         self.model.add(keras_layers.normalization.BatchNormalization())
         self.model.add(keras_layers.Activation('relu'))
         self.model.add(keras_layers.convolutional.MaxPooling2D(pool_size=(2, 2)))
@@ -53,7 +53,7 @@ class AlexNet(BasicModel):
         self.model.add(keras_layers.Dropout(0.5))
 
         # Layer 8
-        self.model.add(keras_layers.Dense(n_classes))
+        self.model.add(keras_layers.Dense(self.nClasses))
         self.model.add(keras_layers.normalization.BatchNormalization())
         self.model.add(keras_layers.Activation('softmax'))
         
