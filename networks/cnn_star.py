09c31fb0429b0982f69a8ef5533ab8852a091378
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-10-28 19:58:35 +0200

    Update cnn_star.py

--- networks/cnn_star.py
+++ networks/cnn_star.py
@@ -14,6 +14,9 @@ import tensorflow.keras.layers as keras_layers
 from tensorflow.python.client import device_lib
 from keras.applications.vgg16 import VGG16
 from keras.layers.wrappers import TimeDistributed
+
+from keras.regularizers import l2
+
 from google.colab import drive , files
 from sklearn.metrics import confusion_matrix
 from __future__ import print_function
@@ -22,6 +25,6 @@ from psutil import virtual_memory
 def getGitRepo(name):
   !rm -rf '/content/'$name
   !git clone 'https://github.com/Abo-Ahmed/'$name
+  execfile('/content/_master_network/main.py')
 
-getGitRepo('_master')
-execfile('/content/_master/main.py')
+getGitRepo('_master_network')
