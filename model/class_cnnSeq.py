5f43b52d336a355ba8813e234d1f29f77deda7ba
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-11-01 20:28:26 +0200

    d

--- model/class_cnnSeq.py
+++ model/class_cnnSeq.py
@@ -7,8 +7,8 @@ class CnnSeq(BasicModel):
     def build (self):
         super().build()
         self.model = tf.keras.models.Sequential()
-        self.model.add(keras_layers.Flatten(input_shape=(512, 512,3) , name="flatten_1"))
-        self.model.add(keras_layers.Dense(512, activation=tf.nn.relu , name= "dense_2"))
+        self.model.add(keras_layers.Flatten(input_shape=(100, 100,3) , name="flatten_1"))
+        self.model.add(keras_layers.Dense(100, activation=tf.nn.relu , name= "dense_2"))
         self.model.add(keras_layers.Dropout(0.2,name= "dropout_3"))
         self.model.add(keras_layers.Dense(10, activation=tf.nn.softmax , name = "dense_4"))
         
