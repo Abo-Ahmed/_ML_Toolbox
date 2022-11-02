68726fde7dba8840a10666200d1a299ff9ca12a6
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-11-02 15:26:16 +0200

    Update class_Transformer.py

--- model/class_Transformer.py
+++ model/class_Transformer.py
@@ -19,7 +19,7 @@ class Transformer(BasicModel):
         self.imageHeight = 128
 
         self.base_path = '/content/drive/MyDrive/eng-mahmoud/dataSet/danbooru2019/'
-        self.images_path = base_path + 'images/'
+        self.images_path = self.base_path + 'images/'
 
         self.s = "safe"
         self.e = "explicit"
@@ -81,9 +81,9 @@ class Transformer(BasicModel):
             proj_output = self.dense_proj(proj_input)
             return self.layernorm_2(proj_input + proj_output)
 
-    def get_compiled_model():
-        sequence_length = MAX_SEQ_LENGTH
-        embed_dim = NUM_FEATURES
+    def get_compiled_model(self):
+        sequence_length = self.MAX_SEQ_LENGTH
+        embed_dim = self.NUM_FEATURES
         dense_dim = 4
         num_heads = 1
         classes = len(label_processor.get_vocabulary())
@@ -103,40 +103,25 @@ class Transformer(BasicModel):
         )
         return model
 
-    def run_experiment():
-        filepath = "/content/video_classifier"
-        checkpoint = keras.callbacks.ModelCheckpoint(
-            filepath, save_weights_only=True, save_best_only=True, verbose=1
-        )
-
-        model = get_compiled_model()
-        history = model.fit(
-            train_array,
-            np.array(train_labeling),
-            validation_split=0.15,
-            epochs=10
-            # callbacks=[checkpoint]
-        )
-
-        # model.load_weights(filepath)
-        _, accuracy = model.evaluate(test_array, np.array(test_labeling))
-        print(f"Test accuracy: {round(accuracy * 100, 2)}%")
-
-        return model
 
     def build (self):
         super().build()
-        self.model = self.get_compiled_model()
-        # self.model = tf.keras.models.Sequential()
-        # inputs = tf.keras.Input(shape=(handler.imageWidth, handler.imageHeight, 3), name="input_1")
-        # t = keras_layers.Flatten(name="flatten_2") (inputs)
-        # t = keras_layers.Dense(handler.imageWidth, activation=tf.nn.relu , name="dense_3") (t)
-        # t = keras_layers.Dropout(0.2,name="dropout_4") (t)
-        # outputs = keras_layers.Dense(10, activation=tf.nn.softmax,name="dense_5") (t)
-        
-        # self.model = tf.keras.Model(inputs , outputs)
-        # self.model.compile(optimizer='adam',
-        #                 loss='sparse_categorical_crossentropy',
-        #                 metrics=['accuracy'])
+        sequence_length = self.MAX_SEQ_LENGTH
+        embed_dim = self.NUM_FEATURES
+        dense_dim = 4
+        num_heads = 1
+        classes = len(label_processor.get_vocabulary())
 
+        inputs = keras.Input(shape=(None, None))
+        x = PositionalEmbedding(
+            sequence_length, embed_dim, name="frame_position_embedding"
+        )(inputs)
+        x = TransformerEncoder(embed_dim, dense_dim, num_heads, name="transformer_layer")(x)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = layers.Dropout(0.5)(x)
+        outputs = layers.Dense(classes, activation="softmax")(x)
+        self.model = keras.Model(inputs, outputs)
 
+        self.model.compile(
+            optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+        )
