d2900356d3b9224de0b644c574a4d5ab87a3a87f
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-11-08 19:13:19 +0200

    Update class_Transformer.py

--- model/class_Transformer.py
+++ model/class_Transformer.py
@@ -93,7 +93,7 @@ class Transformer(BasicModel):
 
         sequence_length = 1
         embed_dim = 50
-        dense_dim = 10
+        dense_dim = 5
         num_heads = 1
         classes = self.nClasses
 
