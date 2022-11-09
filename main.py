d93bfb05bbf733e8a0cf084f48b5383b3ec5f0a0
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-11-09 10:31:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ execfile('/content/_master_network/handler/handler.py')
 handler.intial_config(True, True, '/content/_master_network')
 
 # path ,width , height , batchSize , startBatch , isColored
-handler.dataset_config(handler.ePath, 70, 70, 30, 1, True)
+handler.dataset_config(handler.ePath, 50, 50, 30, 1, True)
 
 handler.read_real()
 # handler.read_sample(None)
@@ -20,6 +20,6 @@ handler.read_real()
 #                     "LstmBi" , "VggBiLstm"  , "Transformer"] )
 
 # handler.special_run("VggBiLstm","program_1")
-handler.special_run("Transformer","program_1")
+handler.special_run("Transformer","program_0")
 
 handler.final_config()
